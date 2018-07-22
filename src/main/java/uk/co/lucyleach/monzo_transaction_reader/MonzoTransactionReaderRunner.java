97c00f3f7917c2277f8eb0fb1896c575796f27e6
Lucy Leach <lucy@lucyleach.co.uk>
2018-07-22 20:26:00 +0100

    Testing response parsing with whoami

--- src/main/java/uk/co/lucyleach/monzo_transaction_reader/MonzoTransactionReaderRunner.java
+++ src/main/java/uk/co/lucyleach/monzo_transaction_reader/MonzoTransactionReaderRunner.java
@@ -4,8 +4,10 @@ import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.StoredCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.*;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.DataStore;
 import com.google.api.client.util.store.FileDataStoreFactory;
@@ -42,9 +44,11 @@ public class MonzoTransactionReaderRunner
     String refreshToken = storedCredential.getRefreshToken();
     String accessToken = storedCredential.getAccessToken();
 
+    HttpTransport httpTransport = new NetHttpTransport();
+    JsonFactory jsonFactory = new JacksonFactory();
     Credential credential = new GoogleCredential.Builder()
-        .setTransport(new NetHttpTransport())
-        .setJsonFactory(new JacksonFactory())
+        .setTransport(httpTransport)
+        .setJsonFactory(jsonFactory)
         .setClientAuthentication(new ClientParametersAuthentication(clientId, clientSecret))
         .setTokenServerUrl(new GenericUrl("https://api.monzo.com/oauth2/token"))
         .build();
@@ -53,8 +57,15 @@ public class MonzoTransactionReaderRunner
 
     try
     {
-      credential.refreshToken();
-      System.out.println("New refresh token: " + credential.getRefreshToken());
+      String whoAmIURL = "https://api.monzo.com/ping/whoami";
+      HttpRequestFactory requestFactory = httpTransport.createRequestFactory(request -> {
+        credential.initialize(request);
+        request.setParser(new JsonObjectParser(jsonFactory));
+      });
+      HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(whoAmIURL));
+      HttpResponse response = request.execute();
+      WhoAmIResponse whoAmIResponse = response.parseAs(WhoAmIResponse.class);
+      int debug = 1;
     } finally
     {
       dataStore.set(dataStoreKey, new StoredCredential(credential));
---
+++ src/main/java/uk/co/lucyleach/monzo_transaction_reader/WhoAmIResponse.java
@@ -0,0 +1,21 @@
+package uk.co.lucyleach.monzo_transaction_reader;
+
+import com.google.api.client.util.Key;
+
+/**
+ * User: Lucy
+ * Date: 22/07/2018
+ * Time: 20:16
+ */
+@SuppressWarnings("WeakerAccess") //Class must be public to be used by Jackson
+public class WhoAmIResponse
+{
+  @Key
+  private boolean authenticated;
+
+  @Key("client_id")
+  private String clientId;
+
+  @Key("user_id")
+  private String userId;
+}
