e166ba5cd93d8b54f698a7e3d3e46a2667a24b56
Lucy <lucy@lucyleach.co.uk>
2018-08-20 17:01:19 +0100

    TagParser slightly easier to read

--- src/main/java/uk/co/lucyleach/monzo_transaction_reader/processor/TagParser.java
+++ src/main/java/uk/co/lucyleach/monzo_transaction_reader/processor/TagParser.java
@@ -3,8 +3,10 @@ package uk.co.lucyleach.monzo_transaction_reader.processor;
 import javax.annotation.Nullable;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toSet;
 
@@ -34,57 +36,79 @@ class TagParser {
   }
 
   private Map<String, Integer> parseMultipleTags(String notes, int totalAmount) throws ParsingException {
-    var notesWithAmount = new HashSet<SplitNote>();
-    var notesWithoutAmount = new HashSet<SplitNote>();
-    for(var note: notes.split(";")) {
-      var trimmedNote = note.trim();
-      var splitNoteArr = trimmedNote.split(" ");
-      if(splitNoteArr.length != 2) {
-        throw new ParsingException("Note " + trimmedNote + " is in incorrect format");
-      }
-
-      String tag;
-      if(!splitNoteArr[1].startsWith("#")) {
-        throw new ParsingException("Note " + trimmedNote + " is in incorrect format");
-      } else {
-        tag = splitNoteArr[1].replaceFirst("#", "");
-      }
-
-      if(splitNoteArr[0].equals("rest")) {
-        notesWithoutAmount.add(new SplitNote(tag, null));
-      } else {
-        try {
-          var positivePoundAmount = Double.parseDouble(splitNoteArr[0]);
-          var amount = (int) Math.round(positivePoundAmount * -100d);
-          notesWithAmount.add(new SplitNote(tag, amount));
-        } catch(NumberFormatException e) {
-          throw new ParsingException(note + " does not start with either rest or an amount");
-        }
-      }
+    var errors = new HashSet<String>();
+    var splitNotes = Stream.of(notes.split(";"))
+        .map(String::trim)
+        .map(note -> createSplitNoteOrNull(note, errors))
+        .filter(Objects::nonNull)
+        .collect(toSet());
+    if(!errors.isEmpty()) {
+      throw new ParsingException("Error(s) occurred reading the notes " + notes + ": " + String.join(", ", errors));
     }
 
-    var totalAmountInNotes = notesWithAmount.stream().mapToInt(sn -> sn.amount).sum();
-    Set<SplitNote> completeNotes;
-    if(notesWithoutAmount.size() > 1) {
-      throw new ParsingException("Cannot have more than 1 \"rest\" note");
-    } else if (notesWithoutAmount.isEmpty()) {
-      if(totalAmount != totalAmountInNotes) {
-        throw new ParsingException("Amounts do not sum to total amount");
-      }
-      completeNotes = notesWithAmount;
-    } else {
-      var restNote = notesWithoutAmount.iterator().next();
-      var restAmount = totalAmount - totalAmountInNotes;
-      completeNotes = new HashSet<>(notesWithAmount);
-      completeNotes.add(new SplitNote(restNote.tag, restAmount));
-    }
+    Set<SplitNote> splitNotesAllWithAmounts = fillInMissingAmountOrFail(totalAmount, splitNotes);
+    checkForDuplicateTags(splitNotesAllWithAmounts);
+    //noinspection ConstantConditions - we know that getAmount won't be null because the method name says so
+    return splitNotesAllWithAmounts.stream().collect(Collectors.toMap(SplitNote::getTag, SplitNote::getAmount));
+  }
 
-    var numDistinctTags = completeNotes.stream().map(SplitNote::getTag).collect(toSet()).size();
-    if(numDistinctTags != completeNotes.size()) {
+  private void checkForDuplicateTags(Set<SplitNote> splitNotesAllWithAmounts) throws ParsingException {
+    var numDistinctTags = splitNotesAllWithAmounts.stream().map(SplitNote::getTag).collect(toSet()).size();
+    if(numDistinctTags != splitNotesAllWithAmounts.size()) {
       throw new ParsingException("Cannot have duplicate tags");
     }
+  }
 
-    return completeNotes.stream().collect(Collectors.toMap(SplitNote::getTag, SplitNote::getAmount));
+  private Set<SplitNote> fillInMissingAmountOrFail(int totalAmount, Set<SplitNote> splitNotes) throws ParsingException {
+    var splitNotesWithAmount = splitNotes.stream().filter(n -> n.getAmount() != null).collect(toSet());
+    var splitNotesWithRest = splitNotes.stream().filter(n -> n.getAmount() == null).collect(toSet());
+    //noinspection ConstantConditions - we know that getAmount won't be null because we've just filtered on that
+    var totalAmountInNotes = splitNotesWithAmount.stream().mapToInt(SplitNote::getAmount).sum();
+    Set<SplitNote> splitNotesAllWithAmounts;
+    if(splitNotesWithRest.isEmpty()) {
+      if(totalAmount != totalAmountInNotes) {
+        throw new ParsingException("Amounts do not sum to total amount");
+      } else {
+        splitNotesAllWithAmounts = splitNotesWithAmount;
+      }
+    } else if(splitNotesWithRest.size() > 1) {
+      throw new ParsingException("Cannot have more than 1 \"rest\" note");
+    } else {
+      var restNote = splitNotesWithRest.iterator().next();
+      var restAmount = totalAmount - totalAmountInNotes;
+      splitNotesAllWithAmounts = new HashSet<>(splitNotesWithAmount);
+      splitNotesAllWithAmounts.add(new SplitNote(restNote.tag, restAmount));
+    }
+    return splitNotesAllWithAmounts;
+  }
+
+  private SplitNote createSplitNoteOrNull(String note, Set<String> errorsToAddTo) {
+    var splitNoteArr = note.split(" ");
+    if(splitNoteArr.length != 2) {
+      errorsToAddTo.add("Note " + note + " is in incorrect format");
+      return null;
+    }
+
+    String tag;
+    if(!splitNoteArr[1].startsWith("#")) {
+      errorsToAddTo.add("Note " + note + " is in incorrect format");
+      return null;
+    } else {
+      tag = splitNoteArr[1].replaceFirst("#", "");
+    }
+
+    if(splitNoteArr[0].equals("rest")) {
+      return new SplitNote(tag, null);
+    } else {
+      try {
+        var positivePoundAmount = Double.parseDouble(splitNoteArr[0]);
+        var amount = (int) Math.round(positivePoundAmount * -100d);
+        return new SplitNote(tag, amount);
+      } catch(NumberFormatException e) {
+        errorsToAddTo.add(note + " does not start with either rest or an amount");
+        return null;
+      }
+    }
   }
 
   class ParsingException extends Exception {
